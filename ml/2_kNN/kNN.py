c59edf2a3147bbeff0c0d9b25aa38d5d1ce1bfdc
XellossRyan <lp8800lz@gmail.com>
2018-11-10 12:08:21 +0800

    add instru
    Signed-off-by: XellossRyan <lp8800lz@gmail.com>

--- ml/2_kNN/kNN.py
+++ ml/2_kNN/kNN.py
@@ -31,18 +31,36 @@ def classify0(inX: array, dataSet: array, labels: array, k: int):
 
 
 def file2matrix(filename):
-    fr = open(filename, "r")#打开文件 
-    arrayOLines = fr.readlines()#读入文件所有行
-    numberOfLines = len(arrayOLines)#识别行数
-    returnMat = np.zeros((numberOfLines, 3))#根据文件的行数和3列数构建一个全零矩阵
-    classLabelVector = []# 记录标签列向量(下面通过获取每行最后一列数作为标签列向量)
-    index = 0
+    #只读模式打开文件 "r"只读模式
+    fr = open(filename, "r")
+    #读入文件所有行
+    arrayOLines = fr.readlines()
+    #识别共有多少行数
+    numberOfLines = len(arrayOLines)
+    #根据文件的行数和3列数构建一个全零矩阵，用于装载文件中的前三列数据
+    returnMat = np.zeros((numberOfLines, 3))
+    # 记录标签列向量(下面通过获取每行最后一列数作为标签列向量)，用于装载文件中第四列类别标签
+    classLabelVector = []
+    index = 0 #
+    #开始遍历每一行数据
     for line in arrayOLines:
+        #strip()砍掉了这个字符串头部和尾部的空格和回车，因为空格和回车没有任何毛用处
         line = line.strip()
+        #split把一个字符串以\t为分隔符，分割成好几个字符串的数组，也就是说listFromLine是一个保存了好几个字符串的数组
+        #比如说第一行数据：40920	8.326976	0.953952	3
+        #数字之间是用的制表符\t分隔的，那么split方法传入\t，告知以\t作为分节符号切分，切分成字符串数组
+        #切分成["40920","8.326976","0.953952","3"]保存在listFromLine中
         listFromLine = line.split("\t")
+        #在returnMat中的第index列开始，保存从文件中读取的数据的前三列数据集
+        #因为数据集文件的前三列是数据，最后一列是结果标签
+        #按照上面的例子，是保存了["40920","8.326976","0.953952"]到returnMat[index]
         returnMat[index] = listFromLine[0:3]
+        #类别标签向量，每一次循环都把listFromLine切分的最后一个值转换成整型值，追加到classLabelVector
+        #按照上面举得例子，是把3追加到classLabelVector后面
         classLabelVector.append(int(listFromLine[-1]))
+        #递增索引，用于读取下一行,直到最后行数值numberOfLines
         index += 1
+    #返回数据集矩阵以及结果集标签向量
     return returnMat, classLabelVector
 
 #import matplotlib.pyplot as plt
