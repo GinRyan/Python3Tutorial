07ba535fe2d614c1e087547f76d376809dcf6a7c
XellossRyan <lp8800lz@gmail.com>
2018-11-13 22:50:11 +0800

    add leetcode

--- leetcode/1_sum_to_target.py
+++ leetcode/1_sum_to_target.py
@@ -17,26 +17,12 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        ret = []
-
-        counts = len(nums)
-        print(nums)
-
-        indexBase = 0
-        indexDiff = 1
-
-        while indexBase < counts:
-            diffVal = target - nums[indexBase]
-            indexDiff = indexBase + 1
-            while indexDiff < counts:
-                if diffVal == nums[indexDiff]:
-                    ret.append(indexBase)
-                    ret.append(indexDiff)
-                    return ret
-                indexDiff += 1
-            indexBase +=1
-     
-        return ret
+        cache = {}
+        for index,num in enumerate(nums):
+            diff = target - num
+            if diff in cache:
+                return [cache[diff],index]
+            cache[num] = index
 
 
 print(Solution().twoSum([7, 2, 11, 15, 4], 6))
